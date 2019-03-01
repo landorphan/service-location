e0f559bbd92832738594fa601a823756ba111696
Timothy Stockstill <timothy.stockstill@cayuse.net>
2019-03-01 15:56:30 -0800

    First Step to add new project for Perf Testing

---
+++ test/Landorphan.Ioc.Collections.Performance.Tests/Interfaces/IRegistrationTarget.cs
@@ -0,0 +1,16 @@
+﻿namespace Ioc.Collections.Performance.Tests
+{
+   using System;
+   using Landorphan.Ioc.ServiceLocation.Interfaces;
+
+   public interface IRegistrationTarget : IIocContainerMetaIdentity, ITargetElapsedTime
+   {
+      Boolean AddPrecludedType(Type precludedType);
+      Boolean RegisterImplementationImplementation(Type fromType, String fromTypeParameterName, String name, Type toType, String toTypeParameterName, Boolean tryLogic);
+      Boolean RegisterInstanceImplementation(Type fromType, String fromTypeParameterName, String name, Object instance, String instanceParameterName, Boolean tryLogic);
+      Boolean RemovePrecludedType(Type precludedType);
+      [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1007:UseGenericsWhereAppropriate")]
+      Boolean ResolveImplementation(Type fromType, String fromTypeParameterName, String name, Boolean tryLogic, out Object instance);
+      Boolean UnregisterImplementation(Type fromType, String name);
+   }
+}
---
+++ test/Landorphan.Ioc.Collections.Performance.Tests/Interfaces/ITargetElapsedTime.cs
@@ -0,0 +1,19 @@
+﻿namespace Ioc.Collections.Performance.Tests
+{
+   using System;
+   using Landorphan.Ioc.ServiceLocation.Interfaces;
+
+   public interface ITargetElapsedTime : IIocContainerConfiguration
+   {
+      void GetRegistrationOverwriteStats(out TimeSpan registrationOverwriteTime, out Int32 registrationOverwriteCount);
+      void GetRegistrationTotalStats(out TimeSpan registrationTotalTime, out Int32 registrationTotalCount);
+      void GetRegistrationValidationStats(out TimeSpan registrationValidationTime);
+      
+      void GetResolutionTotalStats(out TimeSpan resolutionTotalTime, out Int32 resolutionTotalCount);
+      void GetResolutionOverwriteStats(out TimeSpan resolutionOverwriteTime, out Int32 resolutionNewInstancesCount);
+      void GetResolutionValidationStats(out TimeSpan resolutionValidationTime);
+
+      // ReSharper disable IdentifierTypo
+      void GetUnregistrationStats(out TimeSpan unregistrationTotalTime, out Int32 unregistrationTotalCount);
+   }
+}
