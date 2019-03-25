a396ce4b66197cc85d9ab696a69c7029a236e97e
Michael Prochaska <mprochaska@live.com>
2019-03-25 21:33:43 +0000

    warnings as errors on build server confirmed
    removed the offending line

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 
-// Force a warning to be emitted..................................................................................................................................................................................
 [assembly: AssemblyVersion("0.9.0")]
 [assembly: AssemblyFileVersion("0.9.0")]
 [assembly: AssemblyInformationalVersion("0.9.0")]
